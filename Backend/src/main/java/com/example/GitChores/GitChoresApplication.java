b90d8cc4eba42e701ffc886a7232c91b38e3155c
peterlee42 <76502486+peterlee42@users.noreply.github.com>
2025-11-04 11:41:08 -0500

    init project structure

---
+++ Backend/src/main/java/com/example/GitChores/GitChoresApplication.java
@@ -0,0 +1,13 @@
+package com.example.GitChores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitChoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitChoresApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/example/GitChores/GitChoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GitChores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitChoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

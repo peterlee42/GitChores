9d6688e59b0785f69734d1fc9006608dae092fe9
PraneethS42 <spraneeth42@gmail.com>
2025-11-11 01:39:57 -0500

    Controller and Input Boundary Initialization

---
+++ src/main/java/use_case/git_console/GitConsoleInputBoundary.java
@@ -0,0 +1,13 @@
+package use_case.git_console;
+
+/**
+ * Input Boundary for the actions related to the Git Console
+ */
+public interface GitConsoleInputBoundary {
+
+    /**
+     * Executes the git command use case.
+     * @param command the command inputted by the user
+     */
+    void executeCommand(String command);
+}

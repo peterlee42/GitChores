9613ed01782f0f5bdf9edf30ca03354381739256
peterlee42 <76502486+peterlee42@users.noreply.github.com>
2025-11-11 11:37:46 -0500

    user entity basic structure completed

--- src/main/java/entity/Credentials.java
+++ src/main/java/entity/Credentials.java
@@ -4,7 +4,7 @@ package entity;
  * Class representing a user's credentials.
  */
 public class Credentials {
-    private final String userId;
+    private final String username;
     private final String email;
     private final String passwordHash;
 
@@ -12,14 +12,14 @@ public class Credentials {
      * Creates new credentials with the given non-empty username and non-empty
      * password hash.
      *
-     * @param userId       the user ID
+     * @param username     the username
      * @param email        email
      * @param passwordHash the password hash
      * @throws IllegalArgumentException if the password or name are empty
      */
-    public Credentials(String userId, String email, String passwordHash) {
-        if (userId == null || userId.isEmpty()) {
-            throw new IllegalArgumentException("User ID cannot be empty");
+    public Credentials(String username, String email, String passwordHash) {
+        if (username == null || username.isEmpty()) {
+            throw new IllegalArgumentException("Username cannot be empty");
         }
         if (email == null || email.isEmpty()) {
             throw new IllegalArgumentException("Email cannot be empty");
@@ -28,13 +28,13 @@ public class Credentials {
             throw new IllegalArgumentException("Password cannot be empty");
         }
 
-        this.userId = userId;
+        this.username = username;
         this.email = email;
         this.passwordHash = passwordHash;
     }
 
-    public String getUserId() {
-        return userId;
+    public String getUsername() {
+        return username;
     }
 
     public String getEmail() {
--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -4,32 +4,60 @@ package entity;
  * An entity that represents a user in our app.
  */
 public class User {
-    private final String name;
-    private final String passwordHash;
+    private final String id;
+    private String firstName;
+    private String lastName;
+    private final Credentials credentials;
 
     /**
      * Creates a new user with the given non-empty id and non-empty username.
      * 
-     * @param name         the username
-     * @param passwordHash the password hash
-     * @throws IllegalArgumentException if the password or name are empty
+     * @param id          the user ID
+     * @param firstName   the user's first name
+     * @param lastName    the user's last name
+     * @param credentials the user's credentials
+     * @throws IllegalArgumentException if the ID or credentials are invalid
      */
-    public User(String name, String passwordHash) {
-        if (name == null || name.isEmpty()) {
-            throw new IllegalArgumentException("Username cannot be empty");
+    public User(String id, String firstName, String lastName, Credentials credentials) {
+        if (id == null || id.isEmpty()) {
+            throw new IllegalArgumentException("User ID cannot be empty");
         }
-        if (passwordHash == null || passwordHash.isEmpty()) {
-            throw new IllegalArgumentException("Password cannot be empty");
+        if (credentials == null) {
+            throw new IllegalArgumentException("Credentials cannot be null");
         }
-        this.name = name;
-        this.passwordHash = passwordHash;
+        if (firstName == null || firstName.isEmpty()) {
+            throw new IllegalArgumentException("First name cannot be empty");
+        }
+        if (lastName == null || lastName.isEmpty()) {
+            throw new IllegalArgumentException("Last name cannot be empty");
+        }
+        this.id = id;
+        this.credentials = credentials;
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
-    public String getName() {
-        return name;
+    public Credentials getCredentials() {
+        return credentials;
     }
 
-    public String getPasswordHash() {
-        return passwordHash;
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }

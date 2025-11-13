7add159dcb9dc09299d767b87d6511513e3d0a4c
perakasem <pkasemsripitak@gmail.com>
2025-11-13 13:53:16 -0500

    Room entity creaton

---
+++ src/main/java/entity/Room.java
@@ -0,0 +1,58 @@
+package entity;
+
+/**
+ * An entity that represents a room within the app.
+ */
+public class Room {
+
+    private final String id;
+    private String name;
+    private String description;
+    private final String ownerId;
+    private final String inviteCode;
+
+    /**
+     * Creates a new user with the given non-empty id and non-empty username.
+     *
+     * @param id            the room ID
+     * @param name          the room name
+     * @param description   the room description
+     * @param ownerId       the user ID of the room owner
+     * @param inviteCode    the room invite code — TODO: generation
+     * @throws IllegalArgumentException if any of the parameters are null or empty
+     */
+    public Room(String id, String name, String description, String ownerId, String inviteCode) {
+        if (id == null || id.trim().isEmpty()) {
+            throw new IllegalArgumentException("Room ID cannot be null or empty");
+        }
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Room name cannot be null or empty");
+        }
+        if (ownerId == null || ownerId.trim().isEmpty()) {
+            throw new IllegalArgumentException("Owner ID cannot be null or empty");
+        }
+        if (inviteCode == null || inviteCode.trim().isEmpty()) {
+            throw new IllegalArgumentException("Room invite code cannot be null or empty");
+        }
+
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.ownerId = ownerId;
+        this.inviteCode = inviteCode;
+    }
+
+    public String getId() {return id;}
+
+    public String getName() {return name;}
+
+    public String getDescription() {return description;}
+
+    public String getOwnerId() {return ownerId;}
+
+    public String getInviteCode() {return inviteCode;}
+
+    public void setName(String name) { this.name = name; }
+
+    public void setDescription(String description) { this.description = description; }
+}

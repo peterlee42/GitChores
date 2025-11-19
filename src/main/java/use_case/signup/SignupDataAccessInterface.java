0d35927ad2baf028f706e131758cfec148999e2b
peterlee42 <76502486+peterlee42@users.noreply.github.com>
2025-11-19 17:46:15 -0500

    merged with main

---
+++ src/main/java/use_case/signup/SignupDataAccessInterface.java
@@ -0,0 +1,20 @@
+package use_case.signup;
+
+public interface SignupDataAccessInterface {
+    /**
+     * Checks if a username already exists in the data source.
+     *
+     * @param username The username to check.
+     * @return true if the username exists, false otherwise.
+     */
+    boolean usernameExists(String username);
+
+    /**
+     * Saves a new user's credentials to the data source.
+     *
+     * @param username The username of the new user.
+     * @param email    The email of the new user.
+     * @param password The password of the new user.
+     */
+    void saveUserCredentials(String username, String email, String password);
+}

0de1c32d8f74870c9138dbcb720cdfd882a89364
perakasem <pkasemsripitak@gmail.com>
2025-11-20 15:38:08 -0500

    Grid Construction and Rendering

--- src/main/java/view/ActivityTilesPanel.java
+++ src/main/java/view/ActivityTilesPanel.java
@@ -97,7 +97,30 @@ public class ActivityTilesPanel extends JPanel {
     public void addCommitForDate(LocalDate date, String message) {}
 
     @Override
-    protected void paintComponent(Graphics graphics) {}
+    protected void paintComponent(Graphics graphics) {
+        super.paintComponent(graphics);
+
+        final Graphics2D graphics2D = (Graphics2D) graphics;
+        graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        final LocalDate today = LocalDate.now();
+        final LocalDate startDate = today.minusWeeks(WEEKS_TO_SHOW - 1);
+
+        for (int week = 0; week < WEEKS_TO_SHOW; week++) {
+            for (int day = 0; day < DAYS_PER_WEEK; day++) {
+                final LocalDate currentDate = startDate.plusWeeks(week).plusDays(day);
+
+                if (!currentDate.isAfter(today)) {
+                    final int xPosition = week * (TILE_SIZE + TILE_GAP) + TILE_GAP;
+                    final int yPosition = day * (TILE_SIZE + TILE_GAP) + TILE_GAP;
+
+                    final Color tileColor = getColorForActivity(currentDate);
+                    graphics2D.setColor(tileColor);
+                    graphics2D.fillRoundRect(xPosition, yPosition, TILE_SIZE, TILE_SIZE, 2, 2);
+                }
+            }
+        }
+    }
 
     private Color getColorForActivity(LocalDate date) {
         final Integer count = activityData.get(date);

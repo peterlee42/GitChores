19e76e00b45346745ac7fe9498609216de0f0b6c
peterlee42 <76502486+peterlee42@users.noreply.github.com>
2025-11-21 01:06:31 -0500

    signup use case done

--- src/main/java/use_case/signup/SignupDataAccessInterface.java
+++ src/main/java/use_case/signup/SignupDataAccessInterface.java
@@ -1,13 +1,6 @@
 package use_case.signup;
 
 public interface SignupDataAccessInterface {
-    /**
-     * Checks if a username already exists in the data source.
-     *
-     * @param username The username to check.
-     * @return true if the username exists, false otherwise.
-     */
-    boolean usernameExists(String username);
 
     /**
      * Saves a new user's credentials to the data source.

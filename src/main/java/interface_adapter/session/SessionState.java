2e241f94c431f662da4f6b45aadc2e9d8d2aaa2e
peterlee42 <76502486+peterlee42@users.noreply.github.com>
2025-11-27 20:18:23 -0500

    checkstyle fixes

--- src/main/java/interface_adapter/session/SessionState.java
+++ src/main/java/interface_adapter/session/SessionState.java
@@ -1,4 +1,4 @@
-package interface_adapter.logged_in;
+package interface_adapter.session;
 
 public class SessionState {
     private String username;
--- src/main/java/interface_adapter/session/SessionViewModel.java
+++ src/main/java/interface_adapter/session/SessionViewModel.java
@@ -1,4 +1,4 @@
-package interface_adapter.logged_in;
+package interface_adapter.session;
 
 import interface_adapter.ViewModel;
 

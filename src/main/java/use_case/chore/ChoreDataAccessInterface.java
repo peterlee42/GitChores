e0fd682fb1de3f00b08e3fc0abe58f0add3991b6
kiko <147737506+kikotc@users.noreply.github.com>
2025-11-30 04:28:03 -0500

    Added Javadocs comments

--- src/main/java/use_case/chore/ChoreDataAccessInterface.java
+++ src/main/java/use_case/chore/ChoreDataAccessInterface.java
@@ -5,39 +5,45 @@ import java.util.List;
 import entity.Chore;
 
 public interface ChoreDataAccessInterface {
+
     /**
-     * Placeholder.
-     * @param chore placeholder
+     * Creates or updates a chore.
+     *
+     * @param chore the chore we want to save to the database
      */
     void saveChore(Chore chore);
 
     /**
-     * Placeholder.
-     * @param roomId placeholder
-     * @param choreId placeholder
-     * @return Placeholder
+     * Get a chore by its choreID.
+     *
+     * @param roomId the ID of the room
+     * @param choreId the ID of the chore
+     * @return the chore with choreID, or null if not found
      */
     Chore getChoreById(String roomId, String choreId);
 
     /**
-     * Placeholder.
-     * @param roomId placeholder
-     * @return placeholder
+     * Get all the chores for a room.
+     *
+     * @param roomId the ID of the room
+     * @return a list of all the chores in a room
      */
     List<Chore> getChoresForRoom(String roomId);
 
     /**
-     * Placeholder.
-     * @param roomId placeholder
-     * @param userId placeholder
-     * @return placeholder
+     * Get all the chores assigned to a user.
+     *
+     * @param roomId the ID of the room
+     * @param userId the ID of the user
+     * @return a list of all the chores assigned to that user
      */
     List<Chore> getChoresAssignedToUser(String roomId, String userId);
 
     /**
-     * Placeholder.
-     * @param roomId placeholder
-     * @param choreId placeholder
+     * Delete a chore.
+     *
+     * @param roomId the ID of the room
+     * @param choreId the ID of the chore
      */
     void deleteChore(String roomId, String choreId);
 }

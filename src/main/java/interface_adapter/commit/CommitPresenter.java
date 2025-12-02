95fe9a7640049373a46dbd2e6ad5c831232b220b
Peter Lee <76502486+peterlee42@users.noreply.github.com>
2025-12-02 02:17:06 -0500

    Update CommitPresenter.java

--- src/main/java/interface_adapter/commit/CommitPresenter.java
+++ src/main/java/interface_adapter/commit/CommitPresenter.java
@@ -16,7 +16,7 @@ public class CommitPresenter implements CommitOutputBoundary {
         final String formattedTime = time.substring(0, 10) + ", Time " + time.substring(11, 19);
 
         this.viewMessage = String.format(
-                "Commit #%s created: %s at %s",
+                "Commit #%d created: %s at %s",
                 responseModel.getCommitId(),
                 responseModel.getMessage(),
                 formattedTime);
